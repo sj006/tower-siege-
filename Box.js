1d814b5420844f4dec04aa2dab95cfecacc9a1d9
sj006 <74005232+sj006@users.noreply.github.com>
2021-01-05 14:15:18 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,23 @@
+class Box1 {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false,
+          friction:1,
+         restitution:1.1,
+         dencity:4
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+       // var angle = this.body.angle;
+       // translate(this.body.position.x, this.body.position.y);
+      //  rotate(angle);
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("green");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
